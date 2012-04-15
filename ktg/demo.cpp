f1d140df0834af81a338e5c40e0e89ae5f23ff6c
Fabian 'ryg' Giesen <rygorous@gmail.com>
2012-04-14 22:16:27 -0700

    ktg VS2010 fixes

--- ktg/demo.cpp
+++ ktg/demo.cpp
@@ -5,6 +5,8 @@
 /***                                                                      ***/
 /****************************************************************************/
 
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <stdio.h>
 #include "gentexture.hpp"
 
